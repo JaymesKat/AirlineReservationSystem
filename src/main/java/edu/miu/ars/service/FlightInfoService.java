4bd2f7efd72e166bbb5a3af381f266d24e4e5b1f
undranana <26426260+undranana@users.noreply.github.com>
2021-11-14 23:01:34 -0600

    refactored Agent and FlightInfo controllers, services and serviceImpl

---
+++ src/main/java/edu/miu/ars/service/FlightInfoService.java
@@ -0,0 +1,10 @@
+package edu.miu.ars.service;
+
+import edu.miu.ars.domain.Airline;
+import edu.miu.ars.domain.FlightInfo;
+
+import java.util.List;
+
+public interface FlightInfoService extends GenericService<FlightInfo> {
+
+}
