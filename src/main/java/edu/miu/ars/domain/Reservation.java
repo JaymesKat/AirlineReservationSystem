4f83b8670ffb389c12562a152c0e3410ca203b98
torabTech <53045466+torabTech@users.noreply.github.com>
2021-11-14 23:35:08 -0600

    passenger confliction resolved

--- src/main/java/edu/miu/ars/domain/Reservation.java
+++ src/main/java/edu/miu/ars/domain/Reservation.java
@@ -11,13 +11,16 @@ import java.util.List;
 @Getter
 @NoArgsConstructor
 public class Reservation {
-    @Id    @GeneratedValue    private Long id;
+    @Id
+    @GeneratedValue
+    private Long id;
     @Column(length = 6,nullable = false)
     private String code;
+
      //@OneToMany
      //private List<FlightInfo> flightInfoList = new ArrayList<>();
 
-     @OneToMany(mappedBy = "reservation")
+    @OneToMany(mappedBy = "reservation")
     private List<Ticket> ticketList = new ArrayList<>();
     /*  public void addFlightInfo(FlightInfo flightInfo) {
         if (null != flightInfo)
